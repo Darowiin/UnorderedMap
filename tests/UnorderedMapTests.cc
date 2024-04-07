e2bdd0d0d58ad690586f053dabdc9cba7459a850
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2024-04-07 18:39:47 +0400

    done tests

--- tests/UnorderedMapTests.cc
+++ tests/UnorderedMapTests.cc
@@ -18,29 +18,63 @@ TEST(UnorderedMap, Methods1) {
 
     map.print();
 
-    auto item = map.search(2);
-    std::cout << "First value with key = 2: " << *item << std::endl;
-
-    int count = map.count(2);
-    std::cout << "Count of items with key = 2: " << count << std::endl;
-
     auto map2(map);
-    map2.insert_or_assign(12, "Twelve");
-    map2.insert_or_assign(2, "TwoTwoTwo");
-    map2.erase(1);
     map2.print();
 
     auto map3 = map;
-    //map3.print();
-
-    std::cout << "Five in HashTable: " << map.contains("Five") << std::endl;
-    std::cout << "Six in HashTable: " << map.contains("Six") << std::endl;
-
+    map3.print();
+    
     UnorderedMap<int, std::string> map4(10,0,10,0,15);
     map4.print();
+    
+}
 
-    std::cout << std::endl << "LXXV: " << romanToDecimal("LXXV") << std::endl;
-    std::cout << std::endl << "XCIX: " << romanToDecimal("XCIX") << std::endl;
-    std::cout << std::endl << "DXLIX: " << romanToDecimal("DXLIX") << std::endl;
-    std::cout << std::endl << "MCMLXXVI: " << romanToDecimal("MCMLXXVI") << std::endl;
+TEST(UnorderedMap, Methods2) {
+    UnorderedMap<int, std::string> map(10);
+
+    map.insert(1, "One");
+    map.insert(1, "OneOne");
+    map.insert(2, "Two");
+    map.insert(2, "TwoTwo");
+    map.insert(2, "TwoTwoTwo");
+    map.insert(5, "Five");
+    
+    auto item = map.search(2);
+    ASSERT_TRUE(*item == "Two");
+
+    map.insert_or_assign(12, "Twelve");
+    ASSERT_TRUE(map.search(12));
+
+    map.insert_or_assign(2, "TwoTwoTwo");
+    auto item2 = map.search(2);
+    ASSERT_TRUE(*item2 == "TwoTwoTwo");
+
+    
+}
+
+TEST(UnorderedMap, Methods3) {
+    UnorderedMap<int, std::string> map(10);
+
+    map.insert(1, "One");
+    map.insert(1, "OneOne");
+    map.insert(2, "Two");
+    map.insert(2, "TwoTwo");
+    map.insert(2, "TwoTwoTwo");
+    map.insert(5, "Five");
+
+    map.erase(1);
+    map.erase(1);
+    ASSERT_FALSE(map.search(1));
+
+    ASSERT_TRUE(map.count(2) == 3);
+
+    ASSERT_TRUE(map.contains("Five") == 1);
+    ASSERT_TRUE(map.contains("Six") == 0);
+}
+
+TEST(UnorderedMap, Task) {
+    ASSERT_TRUE(romanToDecimal("LXXV") == 75);
+    ASSERT_TRUE(romanToDecimal("XCIX") == 99);
+    ASSERT_TRUE(romanToDecimal("DXLIX") == 549);
+    ASSERT_TRUE(romanToDecimal("MCMLXXVI") == 1976);
 }
